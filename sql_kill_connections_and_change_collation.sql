7df88d085381871bc179acd7e5e460a3053121a4
rtnk <robert.r.nowak@gmail.com>
2017-09-27 09:33:25 +0200

    Create sql_kill_connections_and_change_collation.sql

---
+++ sql_kill_connections_and_change_collation.sql
@@ -0,0 +1,11 @@
+USE [master];
+
+DECLARE @kill varchar(8000) = '';  
+SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'  
+FROM sys.dm_exec_sessions
+WHERE database_id  = db_id('DBNAME')
+
+EXEC(@kill);
+
+ALTER DATABASE DBNAME COLLATE Polish_CI_AS
+GO
